de34114ac4a6b411e0330d9fc638ecb7838ebe47
usb777 <dzsamoila@gmail.com>
2021-07-27 23:26:43 -0400

    if-else on Python

---
+++ if_else.py
@@ -0,0 +1,15 @@
+a  =3
+if a>4 :  print ('a is bigger then 4')
+else : print('a is smaller')
+
+
+
+
+b = -5
+ 
+if b == 0 :
+    print('b is zero.')
+elif b > 0:
+    print('b is positive.')
+elif b < 0:
+    print('b is negative.')
