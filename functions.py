af9fe883ea146440dce20056b07cdf70b345554e
usb777 <dzsamoila@gmail.com>
2021-07-31 13:27:39 -0400

    function

---
+++ functions.py
@@ -0,0 +1,23 @@
+def first():
+  print ("function print")
+
+
+first()  
+
+
+
+
+def funcWithParameter(str):
+    for i in str:
+     print (str, i, " hello!")
+
+
+funcWithParameter("Dzmitry")
+
+
+def summa(a,b):
+   return a+b
+
+print ("summa = ", summa(3,5) )   
+
+
