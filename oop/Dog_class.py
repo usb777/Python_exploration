272ea735bc78577dce095cb1aa97c743fa060cd5
usb777 <dzsamoila@gmail.com>
2021-07-31 14:00:25 -0400

    OOP

---
+++ oop/Dog_class.py
@@ -0,0 +1,24 @@
+class Dog:
+
+    def __init__(self, name, age):  
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print("bark bark!")
+
+    def doginfo(self):
+        print(self.name + " is " + str(self.age) + " year(s) old.")
+
+
+
+
+
+"""dog = new Dog() """      
+ozzy = Dog("Ozzy", 2)
+skippy = Dog("Skippy", 12)
+filou = Dog("Filou", 8)
+
+
+
+ozzy.bark()   # method of Object
