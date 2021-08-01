599f73be12e84534f2c027e58ab6c648fb8c4624
usb777 <dzsamoila@gmail.com>
2021-08-01 12:25:30 -0400

    Loops

---
+++ loops/while.py
@@ -0,0 +1,31 @@
+# The control transfer is transfered  
+# when break statement soon it sees t  
+i = 0  
+str1 = 'javatpoint'  
+
+print('############### break #######################') 
+
+while i < len(str1):   
+    if str1[i] == 't':   
+        i += 1  
+        break 
+    print('Current Letter :', str1[i])   
+    i += 1 
+
+print('############### continue #######################') 
+
+i = 0  
+str1 = 'javatpoint'  
+while i < len(str1):   
+    if str1[i] == 't':   
+        i += 1  
+        continue 
+    print('Current Letter :', str1[i])   
+    i += 1 
+
+print('############### classik number loop #######################') 
+k=0
+while k < 10:   
+  
+    print('Current number is:',k)   
+    k += 1 
--- oop/Dog_class.py
+++ oop/Dog_class.py
@@ -1,15 +1,15 @@
-class Dog:
-  
+class Dog:     
+    
     def __init__(self, name, age):  
         self.name = name
         self.age = age
+          
 
     def bark(self):
         print("bark bark!")
 
     def doginfo(self):
         print(self.name + " is " + str(self.age) + " year(s) old.")
-
 
 
 
