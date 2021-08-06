18a8ccc7910ead71f9f311e58e73561ffebc753e
usb777 <dzsamoila@gmail.com>
2021-08-05 23:53:05 -0400

    dateTime format

---
+++ date/dateFormatChecking.py
@@ -0,0 +1,24 @@
+import datetime
+
+dt  = datetime.datetime.now() 
+
+
+print (dt.strftime ("%d / %m / %Y ")) # represents the date in DD/ MM/ YYYY  
+
+print (dt.strftime ("%Y-%m-%d")) # represents the date in DD/ MM/ YYYY 
+print (dt.strftime (" %I: %M: %S %p ")) # represents the time in HH: MM: SS AM/ PM  
+print (dt.strftime ("%a, %b %d, %Y ")) # represents the day, month date and year 
+
+
+
+
+strDate = dt.strftime ("%Y-%m-%d")
+
+print ("Today date is",strDate )
+
+
+
+
+
+
+#https://medium.com/@ajeet214/conversion-between-datetime-object-and-string-in-python-b1bc527a18
