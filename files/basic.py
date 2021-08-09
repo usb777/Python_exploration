cf6c2e769f7492ada102294177263760c6c93ffb
usb777 <dzsamoila@gmail.com>
2021-08-08 23:58:59 -0400

    file append

--- files/basic.py
+++ files/basic.py
@@ -1,3 +1,9 @@
-import file
 
-print ("read file")
+fileptr = open("file2.txt", "a")  
+  
+# appending the content to the file  
+fileptr.write(''' 1Python is the modern day language. It makes things so simple. 
+It is the fastest-growing programing language''')  
+  
+# closing the opened the file  
+fileptr.close()  
