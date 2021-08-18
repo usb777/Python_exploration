f5d39108f21ca60c6b5835fa3c5930626f584f7a
usb777 <dzsamoila@gmail.com>
2021-08-17 23:54:04 -0400

    oop

---
+++ oop/Employee.py
@@ -0,0 +1,11 @@
+class Employee:    
+    id = 10   
+    name = "Dzmitry Samoila"    
+    def display (self):    
+        print(self.id,self.name)   
+
+
+
+
+emp = Employee()
+emp.display()       
