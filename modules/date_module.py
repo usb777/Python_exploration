04cd7d50e385ff11371adf285eb263f81647fe14
usb777 <dzsamoila@gmail.com>
2021-08-29 23:54:38 -0400

    modules

--- modules/date_module.py
+++ modules/date_module.py
@@ -1,9 +1,11 @@
 import datetime
 
-def display(str = "hello"):
-    print("hello! "+str)
+def display(str = "hello", msg="forever young"):
+    print("hello! "+str+" "+msg)
 
 
 
 display()
 display("Dzmitry")
+
+
