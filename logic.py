12163f44a6902a7046e4b0007f00fbf12c3d34f1
usb777 <dzsamoila@gmail.com>
2021-09-09 00:01:21 -0400

    logic

--- logic.py
+++ logic.py
@@ -1,6 +1,13 @@
 a = True
 b = False
 
+print(' AND ')
 print('a and a = ',a and a)
 print('a and b = ',a and b)
 print('b and b = ',b and b)
+
+
+print(' OR ')
+print('a or a = ',a or a)
+print('a or b = ',a or b)
+print('b or b = ',b or b)
