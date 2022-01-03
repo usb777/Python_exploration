8cf9e8e310cc08fd0f6af25f61b69b7bfc42e77f
usb777 <dzsamoila@gmail.com>
2022-01-03 17:04:55 -0500

    new python scripts

---
+++ stringa.py
@@ -0,0 +1,15 @@
+quantity = 3  #0
+itemno = 567   #1
+price = 49.95   #2
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
+
+
+name = "Dzmitry"
+lastname= "Samoila"
+age  =34
+race = "white"
+
+
+strok = "My name is {0} lastname {1}  race: {3}  age: {2}"
+print(strok.format(name, lastname, age, race))
