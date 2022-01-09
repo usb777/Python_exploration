c4c0a5146558d6a35f9bc284a71ba1932f19a9d8
usb777 <dzsamoila@gmail.com>
2022-01-09 12:29:04 -0500

    changes

--- if_else.py
+++ if_else.py
@@ -20,6 +20,18 @@ def chooser(a=5):
   elif (a>5): print (str(a)+" more then 5")
   elif (a<5): print (str(a)+" less then 5")
 
-
+def chooser_return(a=5):
+  if a==5:
+          print (str(a)+" equal 5")
+          
+  elif (a>5):
+          print (str(a)+" more then 5")
+          a=a+1
+  elif (a<5):
+          print (str(a)+" less then 5")
+          a=a-1
+  return a
+  
   
 chooser(10)
+print("a stay "+str(chooser_return(100)))
